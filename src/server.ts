18c02afee042638a25bfbcbda5e012892388b19f
Ruan Melo <ruantmelo@gmail.com>
2021-10-23 16:34:13 -0300

    fix message on start

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,3 @@
 import { serverHttp } from "./app";
 
-serverHttp.listen(4000, () => console.log("Server is running on port " + 400))
+serverHttp.listen(4000, () => console.log("Server is running on port " + 4000))
